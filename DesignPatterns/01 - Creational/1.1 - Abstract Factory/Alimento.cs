89560a8e3f49247e38b7428d68ea579f3adb5128
Alexandre Campos <alexandrecamposmon@hotmail.com>
2021-01-05 03:18:04 -0300

    iniciacao classes alimento e panela

---
+++ DesignPatterns/01 - Creational/1.1 - Abstract Factory/Alimento.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.Abstract_Factory
+{
+    public abstract class Alimento
+    {
+        protected Alimento(string nome, TipoAlimento tipoAlimento)
+        {
+            Nome = nome;
+            TipoAlimento = tipoAlimento;
+        }
+        public string Nome { get; set; }
+        public TipoAlimento TipoAlimento { get; set; }
+
+    }
+
+    public enum TipoAlimento
+    {
+        Agridoce,
+        Doce,
+        Salgado
+    }
+
+    public class AlimentoAgridoce : Alimento
+    {
+        public AlimentoAgridoce(string nome, TipoAlimento tipoAlimento) : base(nome, tipoAlimento)
+        {
+        }
+    }
+    public class AlimentoDoce : Alimento
+    {
+        public AlimentoDoce(string nome, TipoAlimento tipoAlimento) : base(nome, tipoAlimento)
+        {
+        }
+    }
+    public class AlimentoSalgado : Alimento
+    {
+        public AlimentoSalgado(string nome, TipoAlimento tipoAlimento) : base(nome, tipoAlimento)
+        {
+        }
+    }
+
+    public class AlimentoCreator
+    {
+        public static Alimento Criar(string nome, TipoAlimento tipoAlimento)
+        {
+            switch (tipoAlimento)
+            {
+                case TipoAlimento.Agridoce:
+                    return new AlimentoAgridoce(nome, tipoAlimento);
+                case TipoAlimento.Doce:
+                    return new AlimentoDoce(nome, tipoAlimento);
+                case TipoAlimento.Salgado:
+                    return new AlimentoSalgado(nome, tipoAlimento);
+                default:
+                    return new AlimentoSalgado(nome, tipoAlimento);
+            }
+        }
+    }
+
+}
---
+++ DesignPatterns/01 - Creational/1.1 - Abstract Factory/Panela.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.Abstract_Factory
+{
+    public abstract class Panela
+    {
+        protected Panela(TipoAlimento tipoAlimento)
+        {
+            TipoAlimento = tipoAlimento;
+        }
+        public TipoAlimento TipoAlimento { get; set; }
+        public abstract void Cozinhar(Alimento alimento);
+    }
+    public class PanelaForma : Panela //Para agridoce
+    {
+        public PanelaForma(TipoAlimento tipoAlimento) : base(tipoAlimento)
+        {
+        }
+
+        public override void Cozinhar(Alimento alimento)
+        {
+            Console.WriteLine("Cozinhando uma comida agridoce - Nome do prato: " + alimento.Nome);
+        }
+    }
+    public class PanelaPipoqueira : Panela //Para doce
+    {
+        public PanelaPipoqueira(TipoAlimento tipoAlimento) : base(tipoAlimento)
+        {
+        }
+
+        public override void Cozinhar(Alimento alimento)
+        {
+            Console.WriteLine("Cozinhando uma comida doce - Nome do prato: " + alimento.Nome);
+        }
+    }
+    public class PanelaFrigideira : Panela //Para salgado
+    {
+        public PanelaFrigideira(TipoAlimento tipoAlimento) : base(tipoAlimento)
+        {
+        }
+
+        public override void Cozinhar(Alimento alimento)
+        {
+            Console.WriteLine("Cozinhando uma comida salgada - Nome do prato: " + alimento.Nome);
+        }
+    }
+
+    public class PanelaCreator
+    {
+        public static Panela Criar(TipoAlimento tipoAlimento)
+        {
+            switch (tipoAlimento)
+            {
+                case TipoAlimento.Agridoce:
+                    return new PanelaForma(tipoAlimento);
+                case TipoAlimento.Doce:
+                    return new PanelaPipoqueira(tipoAlimento);
+                case TipoAlimento.Salgado:
+                    return new PanelaFrigideira(tipoAlimento);
+                default:
+                    return new PanelaFrigideira(tipoAlimento);
+            }
+        }
+    }
+}
